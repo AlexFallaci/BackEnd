a7f8de46077bca2ff10a415039c3e95ca4e3bd3e
Teste <alexsander_fallaci@hotmail.com>
2022-06-02 18:26:33 -0300

    Inserindo a Aula11 no github

---
+++ Java/ExerciciosAula/Aula12/src/br/com/stefanini/estruturacontrole/IfElse.java
@@ -0,0 +1,32 @@
+package br.com.stefanini.estruturacontrole;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int idade = 17;
+		
+		if (idade >= 18) {
+			System.out.println("Maior de idade");
+		}
+		
+		if (idade >= 18) {
+			System.out.println("Maior de idade");
+		}else {
+			System.out.println("Menor de idade");
+		}
+		
+		if (idade >= 199) {
+			System.out.println("Virou mumia");
+		}else if (idade >=18){
+			System.out.println("Maior de idade");
+		}else {
+			System.out.println("Menor de idade");
+		}
+		
+		if (idade >= 18) System.out.println("Maior de idade");
+		
+		
+
+	}
+
+}
---
+++ Java/ExerciciosAula/Aula12/src/br/com/stefanini/estruturacontrole/Loop.java
@@ -0,0 +1,41 @@
+package br.com.stefanini.estruturacontrole;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		int count = 101;
+		
+//		while (count < 100) {
+//			System.out.println("Count: " + count);
+//			count++;
+//		}
+		
+		int countDois = 101;
+		
+//		do {
+//			System.out.println(countDois);
+//			countDois++;
+//		}while(countDois < 100);
+		
+//		for normal
+//		for (int i = 0; i < 100 ; i++) {
+//			System.out.println(i);
+//		}
+		
+		//for com areas ausentes
+		int i = 0;
+		for (; i < 100 ;) {
+			System.out.println(i);
+			i++;
+		}
+		
+		//for com todas areas ausentes (usado para looping infinito)
+//		for (;;) {
+//			System.out.println("for");
+//		}
+		
+		
+
+	}
+
+}
