9d8b684c8fa59af098f7e82f5c01c7a0d17fc27d
Teste <alexsander_fallaci@hotmail.com>
2022-06-06 18:40:02 -0300

    Inserindo a Aula13 no github

---
+++ Java/ExerciciosAula/Aula13/src/br/com/stefanini/estruturasdecontrole/IfElse.java
@@ -0,0 +1,37 @@
+package br.com.stefanini.estruturasdecontrole;
+
+public class IfElse {
+	
+	public static void main(String[] args) {
+		
+		int nota = 7;
+		
+		//if
+		if(nota >= 7) {
+			System.out.println("Aprovado");
+		}
+		
+		//if sem bloco (não recomendado)
+		if(nota >= 7) System.out.println("Aprovado");
+		
+		//if else
+		
+		if(nota >= 7) {
+			System.out.println("Aprovado");
+		}else {
+			System.out.println("Reprovado");
+		}
+		
+		//if else if
+		if(nota >= 9) {
+			System.out.println("Aprovado");
+			System.out.println("Parabéns");
+		}else if(nota >= 7){
+			System.out.println("Aprovado");
+		}else {
+			System.out.println("Reprovado");
+		}
+		
+	}
+
+}
